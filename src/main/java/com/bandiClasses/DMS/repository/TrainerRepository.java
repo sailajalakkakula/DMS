7459e1c57db69a8884cab04fc754d89127f70b0b
Varshakalva <kalvakuntla.varsha@gmail.com>
2023-11-14 16:35:07 -0600

    first commit

---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+/**
+ * @author s559976
+ * Nov 13, 2023
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
